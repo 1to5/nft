595515335914a2c43f66fa9759aabb26a247b5d0
vox <vox@upmie.com>
2022-03-29 18:16:38 +0800

    fix custom font

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,9 @@ export const links: LinksFunction = () => {
   return [
     { rel: "stylesheet", href: fontStylesheetUrl },
     { rel: "stylesheet", href: tailwindStylesheetUrl },
+    { rel: "preconnect", href: 'https://fonts.googleapis.com'},
+    { rel: "preconnect", href: 'https://fonts.gstatic.com', crossOrigin: 'anonymous'},
+    { rel: "stylesheet", href: 'https://fonts.googleapis.com/css2?family=Quicksand&display=swap'},
   ];
 };
 
