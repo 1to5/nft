efaea64b52eabe07e881b8ec75cc778fc1e0e2b2
Elbert-Ainstein <83158868+Elbert-Ainstein@users.noreply.github.com>
2022-03-30 10:33:48 +0800

    som smol shit

--- app/routes/navbar.jsx
+++ app/routes/navbar.jsx
@@ -17,10 +17,17 @@ export default function Nav() {
                     Undecided
                 </p>
             </Link>
+            {/* <label class="relative block">
+                <span className="sr-only">Search</span>
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                    <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
+                </span>
+                <input className="placeholder:italic placeholder:text-blue-900 block bg-blue-100 w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm my-auto" placeholder="Search..." type="text" name="search"/>
+            </label> */}
             <input 
             type="text" 
             placeholder="Search..." 
-            className="h-12 w-full bg-blue-300 border-black dark:bg-blue-600 text-blue-50 rounded-full focus:outline-none focus:shadow-outline"
+            className="block ml-32 h-10 w-1/3 font-quicksand placeholder:text-gray-50 bg-blue-500 border-black text-[#FFFFFF] py-2 pl-9 rounded-full focus:outline-none focus:shadow-outline my-auto"
             />
         </nav>
     );
