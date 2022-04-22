b7fa90e690ed5400a2f3e1d7fa88c81bb035f254
Elbert-Ainstein <iwu48@yahoo.com>
2022-04-22 09:34:04 +0800

    lol

--- app/routes/create/index.tsx
+++ app/routes/create/index.tsx
@@ -18,12 +18,18 @@ export default function Create() {
     
     <main>
       <div className='min-h-screen bg-blue-100 dark:bg-blue-600'>
+        <h1 className='text-4xl pt-8 pl-24 text-gray-900 font-quicksand font-bold dark:text-gray-50 '>
+          Create your Item
+        </h1>
         <div className='flex mx-auto'>
-          <button 
+          <div className='my-auto'>
+             
+          </div>
+          {/* <button 
           onClick={handleClick}
           className='bg-blue-50 shadow-lg opacity-50 dark:bg-blue-500 rounded-xl h-64 w-64 '>
             UPLOAD UR SHIT
-          </button>
+          </button> */}
         </div>
       </div>
     </main>
