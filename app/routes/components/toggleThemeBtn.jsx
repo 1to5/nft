2419eabc4f7633c7bfd832d5239d05eb824b0519
Elbert-Ainstein <83158868+Elbert-Ainstein@users.noreply.github.com>
2022-04-25 16:53:58 +0800

    random shit i wanna commit

--- app/routes/components/toggleThemeBtn.jsx
+++ app/routes/components/toggleThemeBtn.jsx
@@ -4,8 +4,12 @@ import {RiSunLine} from 'react-icons/ri';
 export default function ToggleButton({toggleTheme}) {
     return(
         <div className=' 2xl:p-0 lg:p-6 w-16 h-auto'>
-            <div className='' onClick={toggleTheme}>
-                <RiSunLine className='h-8 w-8 fill-gray-300 hover:fill-gray-800 duration-100 dark:fill-gray-50 dark:hover:fill-gray-300 ease-in' />
+            <div 
+            className='' 
+            
+            onClick={toggleTheme}>
+                <RiSunLine 
+                className='transition hover:-translate-y-1 hover:scale-110 delay-150 h-8 w-8 fill-gray-300 hover:fill-gray-800 duration-300 dark:fill-gray-50 dark:hover:fill-gray-300 ease-in-out' />
             </div>
         </div>
     )
